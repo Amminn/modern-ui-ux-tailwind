2845a4aedce585dbda5082e0f40777939cb08606
Amminn <vrb.nnn@gmail.com>
2022-10-11 20:32:01 +0100

    feat: import all components to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import styles from './styles'
 
+import { Navbar, Hero, Stats ,Business ,Billing ,CardDeal ,Testimonials ,Clients ,CTA ,Footer } from './components'
+
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
